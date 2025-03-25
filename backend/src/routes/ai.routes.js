b506f9968aa58bf0e95bcc581c7f6268044bfc22
Prince <princevirmani410@gmail.com>
2025-03-25 17:57:12 +0530

    deploying

---
+++ backend/src/routes/ai.routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const aiController = require("../controllers/ai.controller");
+const router = express.Router();
+
+// this route.get(("route", controller))
+
+router.post("/get-review", aiController.getReview);
+
+module.exports = router;
